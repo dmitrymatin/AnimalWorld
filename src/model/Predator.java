7118eaa5fdcae3191aabb098427e3f19dfa2302b
Dmitry Matin <super.dmitry320@yandex.ru>
2020-09-24 22:25:07 +0300

    Add Predator.hunt method that uses randomisation functionality

--- src/model/Predator.java
+++ src/model/Predator.java
@@ -1,8 +1,19 @@
 package model;
 
+import java.util.Random;
+
 public class Predator extends Animal {
 
     public Predator(String name, float m) {
         super(name, m);
     }
+
+    public void hunt(Animal animal) {
+        Random random = new Random();
+        boolean success = random.nextBoolean();
+        if (success) {
+            this.eat(animal);
+        }
+    }
+
 }
