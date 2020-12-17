cca20112e3eec643b37e9f8e361fb26e5ab91ad3
Dmitry Matin <super.dmitry320@yandex.ru>
2020-12-17 15:25:42 +0300

    Add logging

---
+++ src/app/FormLogger.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.awt.*;
+
+public class FormLogger implements Logger {
+
+    private TextArea textArea;
+
+    public FormLogger(TextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    public void logMessage(String message) {
+        textArea.setText("\n" + message);
+    }
+}
---
+++ src/app/Logger.java
@@ -0,0 +1,5 @@
+package app;
+
+public interface Logger {
+    void logMessage(String message);
+}
